e428f9dc5789b4e233b350aaed9352638a377374
Jacknb03 <3124421733@qq.com>
2026-04-23 20:17:22 +0800

    Finish Task 1

--- CW2/Script_template.m
+++ CW2/Script_template.m
@@ -5,8 +5,19 @@
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
-% Insert answers here
+clear a; % clear possible old connection
+a = arduino(); % connection
+disp('Arduino connected！');
 
+disp('testing green light shinning...');
+% shinning 5 times， 0.5s open 0.5s close
+for i = 1:5
+    writeDigitalPin(a, 'D2', 1); % light
+    pause(0.5);                  % wait 0.5秒
+    writeDigitalPin(a, 'D2', 0); % 0 shut down
+    pause(0.5);
+end
+disp('test is done');
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
 % Insert answers here

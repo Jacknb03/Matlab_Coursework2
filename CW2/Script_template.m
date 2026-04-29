7964c57538bd7b35dff59b476baaa85ea0d2287a
Jacknb03 <3124421733@qq.com>
2026-04-29 22:21:19 +0800

    correct Task1

--- CW2/Script_template.m
+++ CW2/Script_template.m
@@ -6,25 +6,57 @@
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
 clear a; % clear possible old connection
-a = arduino(); % connection
+a = arduino('COM8', 'Uno'); % connection
 disp('Arduino connected！');
 
 disp('testing green light shinning...');
-% shinning 5 times， 0.5s open 0.5s close
+% shinning 5 times， 0.5s on 0.5s off
 for i = 1:5
-    writeDigitalPin(a, 'D2', 1); % light
-    pause(0.5);                  % wait 0.5秒
-    writeDigitalPin(a, 'D2', 0); % 0 shut down
-    pause(0.5);
+    writeDigitalPin(a, 'D2', 1); % on
+    pause(0.5);                  % wait 0.5s
+    writeDigitalPin(a, 'D2', 0); % shut down
+    pause(0.5);           %keep 0.5s
 end
 disp('test is done');
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
-% Insert answers here
+%set parameters
+duration = 600; % 600s total
+num_samples = duration + 1;     % Read (n+1) data (0s,1s...600s)
+
+%initialize time&temp arrays
+time_array = zeros(1, num_samples); 
+temp_array = zeros(1, num_samples); 
+
+%choose analog pin'A0‘
+sensor_pin = 'A0';
+
+
+% collect data
+for i = 1:num_samples
+
+    %read voltage on A0
+    v = readVoltage(a, sensor_pin);
+    
+    % trsform voltage into temperature
+    temp_array(i) = (v - 0.5) / 0.01;
+    time_array(i) = i - 1;      % record current time(s)
+    
+    pause(1); % pause 1s 
+end
+
+
+disp('开始采集温度数据，请不要拔掉线缆...');
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
-% Insert answers here
+% duration=600; %acquisition time(s)
+% 
+% while duration >0
+%     A0_voltage= read_voltage(a,'A0')
+%     pause(1)
+%     duration=duration-1;
+    
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
